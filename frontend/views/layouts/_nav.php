ce289b79188cc8dbe296673acbcccf929f6ec0e6
sultanroomcode <sultanroomcode@gmail.com>
2020-09-13 00:05:17 +0700

    change to url friendly

--- frontend/views/layouts/_nav.php
+++ frontend/views/layouts/_nav.php
@@ -1,6 +1,9 @@
+<?php 
+use yii\helpers\Url;
+?>
 <div class="sidebar" data-color="purple" data-image="../assets/img/sidebar-1.jpg">
         <div class="logo">
-            <a href="http://www.creative-tim.com" class="simple-text">
+            <a href="<?= Url::base(true).'/' ?>" class="simple-text">
             <?= Yii::$app->name ?>
             </a>
         </div>
@@ -9,49 +12,49 @@
         <div class="sidebar-wrapper">
             <ul class="nav">
                 <li>
-                    <a href="dashboard.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">dashboard</i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li>
-                    <a href="user.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">person</i>
                         <p>User Profile</p>
                     </a>
                 </li>
                 <li class="active">
-                    <a href="table.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">content_paste</i>
                         <p>Table List</p>
                     </a>
                 </li>
                 <li>
-                    <a href="typography.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">library_books</i>
                         <p>Typography</p>
                     </a>
                 </li>
                 <li>
-                    <a href="icons.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">bubble_chart</i>
                         <p>Icons</p>
                     </a>
                 </li>
                 <li>
-                    <a href="maps.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">location_on</i>
                         <p>Maps</p>
                     </a>
                 </li>
                 <li>
-                    <a href="notifications.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons text-gray">notifications</i>
                         <p>Notifications</p>
                     </a>
                 </li>
                 <li class="active-pro">
-                    <a href="upgrade.html">
+                    <a href="<?= Url::base(true).'/dashboard' ?>">
                         <i class="material-icons">unarchive</i>
                         <p>Upgrade to PRO</p>
                     </a>
